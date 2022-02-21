eb2faea8d7c898f21dc9686e266f5aa864934224
frank255 <ilungafrank3@gmail.com>
2022-02-21 14:07:43 +0300

    changes

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -6,40 +6,46 @@ use App\Http\Resources\PetitionCollection;
 use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PetitionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return PetitionCollection
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
 
-        return new PetitionCollection(Petition::all());
+        return response()->json( new PetitionCollection(Petition::all()),
+            Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function store(Request $request)
     {
-        //
+        $petition = Petition::create($request->only([
+            'title','description','category','author','signees'
+        ]));
+
+        return  new PetitionResource($petition);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\petition  $petition
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function show(petition $petition)
     {
-        //
+       return new PetitionResource($petition);
     }
 
     /**
@@ -47,21 +53,27 @@ class PetitionController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\petition  $petition
-     * @return \Illuminate\Http\Response
+     * @return PetitionResource
      */
     public function update(Request $request, petition $petition)
     {
-        //
+        $petition->update($request->only([
+            'title','description','category','author','signees'
+        ]));
+
+        return  new PetitionResource($petition);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\petition  $petition
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(petition $petition)
     {
-        //
+        $petition->delete();
+
+        return  response()->json(null,Response::HTTP_NO_CONTENT);
     }
 }
